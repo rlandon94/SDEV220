0ed463c445c0c7fc8eaa4f2bbe2345bc3361ee4e
rlandon94 <143028206+rlandon94@users.noreply.github.com>
2023-08-26 14:29:25 -0400

    Add files via upload

---
+++ SDEV220_PA_1.py
@@ -0,0 +1,15 @@
+s = 60
+
+m = 60
+
+seconds_per_hour =  s * m
+
+seconds_per_day = seconds_per_hour * 24
+
+print(seconds_per_day)
+
+print(seconds_per_day / seconds_per_hour)
+
+print(seconds_per_day // seconds_per_hour)
+
+#using floating-point division and integer division did result in the same outcome aside from the final .0
